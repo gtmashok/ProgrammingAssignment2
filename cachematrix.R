108e246c62d9ef6edf7b8df3ec3afcd3d22ee5b5
Gautam Ashok <gtmashok@gmail.com>
2015-02-19 23:23:58 -0800

    cachematrix file now contains the two paired functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This 'cachematrix' function is created for Programming Assignment 2
+## for the R Programming course. It comprises of two paired functions 
+## that determine the inverse of a matrix (especially large ones) by 
+## caching the inverse of the original 
 
-## Write a short comment describing this function
+## Creates special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setcache <- function(solve) s <<- solve
+        getcache <- function() s
+        list(set = set, get = get,
+             setcache = setcache,
+             getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## Computes inverse of the matrix returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getcache()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setcache(s)
+        s
 }
